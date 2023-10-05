cc82046769c361204b6f6cf38d6c3aeebb64a284
Monchez9 <104701226+Monchez9@users.noreply.github.com>
2023-10-04 22:56:38 -0600

    ataque
    ataque basicpo del jugador

--- Jumping Stardust Crusader/Assets/Scrpits/combate.cs
+++ Jumping Stardust Crusader/Assets/Scrpits/combate.cs
@@ -7,6 +7,11 @@ public class combate : MonoBehaviour
   [SerializeField] private float radioGolpe;
   [SerializeField] private Transform controladorGolpe;
   [SerializeField] private float dannoGolpe; 
+  private Animator animator;
+
+  private void Start() {
+    animator = GetComponent<Animator>();
+  }
 
   private void Update() {
     if(Input.GetButtonDown("Fire1")) {
@@ -15,6 +20,7 @@ public class combate : MonoBehaviour
   }
 
   private void Golpe(){
+    animator.SetTrigger("Golpe");
     Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position
       , radioGolpe);
     foreach(Collider2D colisionador in objetos) {
