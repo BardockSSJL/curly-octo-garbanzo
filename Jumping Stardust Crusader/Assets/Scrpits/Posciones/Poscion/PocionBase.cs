7841a1e46dd73500fca2cd94d9dd47f840c684fc
BardockSSJL <frayaal27@hotmail.com>
2023-11-09 01:06:36 -0600

    Pociones se acercan al jugador automáticamente

--- Jumping Stardust Crusader/Assets/Scrpits/Posciones/Poscion/PocionBase.cs
+++ Jumping Stardust Crusader/Assets/Scrpits/Posciones/Poscion/PocionBase.cs
@@ -6,7 +6,31 @@ namespace Pociones
     public abstract class PocionBase : MonoBehaviour
     {
         [SerializeField] protected string id;
+        //[SerializeField] protected PocionDeteccionJugador AreaDeteccionJugador;
+
+        protected CircleCollider2D AreaDeteccionJugador;
 
         public string Id=>id;
+
+        void Awake() {
+            AreaDeteccionJugador = gameObject.AddComponent<CircleCollider2D>();
+            AreaDeteccionJugador.radius = 1.5f;
+            AreaDeteccionJugador.isTrigger = true;
+            RigidbodyConstraints2D a = RigidbodyConstraints2D.FreezeRotation;
+            gameObject.GetComponent<Rigidbody2D>().constraints = a;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.3f;
+        }
+
+        void OnTriggerStay2D(Collider2D otro) {
+            if (otro.gameObject.tag == "Jugador") {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(
+                    (otro.gameObject.transform.position - transform.position).normalized * 7f);
+            }
+        }
+
+        private void OnDrawGizmos() {
+            Gizmos.DrawWireSphere(transform.position, 1.5f);
+        }
     }
+
 }
