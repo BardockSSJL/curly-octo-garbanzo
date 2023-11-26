32371581c710924e3388c3bff1eee3691b1c7ece
Monchez9 <104701226+Monchez9@users.noreply.github.com>
2023-11-26 17:35:17 -0600

    siguiente niveles

---
+++ Jumping Stardust Crusader/Assets/sigNivel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sigNivel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D colision)
+    {
+        if(colision.gameObject.tag == "Jugador"){
+            SceneManager.LoadScene(2);
+        }
+    }
+}
