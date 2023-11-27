a7c835546f74c27d7235db26d582e3fe6accf40b
Monchez9 <104701226+Monchez9@users.noreply.github.com>
2023-11-26 18:33:13 -0600

    2 niveles listos

---
+++ Jumping Stardust Crusader/Assets/zonaMuerte.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class zonaMuerte : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D colision)
+    {
+        if(colision.gameObject.tag == "Jugador"){
+            
+            SceneManager.LoadScene(2);
+        }
+    }
+}
+
+
